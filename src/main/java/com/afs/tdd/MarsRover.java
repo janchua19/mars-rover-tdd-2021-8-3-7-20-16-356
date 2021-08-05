ffcd015b3012bb4eefef4baa3f2dbffae87b3193
janchua19 <jan.edward.chua@oocl.com>
2021-08-05 17:50:43 +0800

    [MarsRover][Jan] feat: implements should_return_locationX_0_locationY_1_direction_N_when_execute_command_given_locationX_0_locationY_0_direction_N_and_M

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -8,6 +8,16 @@ public class MarsRover {
     }
 
     public void executeCommand(String command) {
+        if(command.equals("M")){
+            move();
+        }
+    }
+
+    private void move() {
+        final int locationX = roverStatus.getLocationX();
+        final int locationY = roverStatus.getLocationY();
+        final String direction = roverStatus.getDirection();
+        roverStatus = new RoverStatus(locationX,locationY + 1, direction);
     }
 
     public RoverStatus getRoverStatus() {
